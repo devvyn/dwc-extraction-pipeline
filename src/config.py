38cf86895c769292a85e8c7e3889e11e9eff0fb0
devvyn <me@devvyn.ca>
2025-07-31 14:43:55 -0600

    remove preprocessing with tesseract

--- src/config.py
+++ src/config.py
@@ -6,5 +6,4 @@ load_dotenv()
 
 class Config:
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    OCR_THRESHOLD = float(os.getenv('OCR_THRESHOLD', '0.85'))
     MODE = os.getenv('APP_MODE', 'local')
